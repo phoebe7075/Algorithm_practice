c6680f06030f3ce82c7639ad0ad5d6ad2c225af7
Herase <phoebe7075@gmail.com>
2021-12-25 15:19:26 +0900

    very hard mode 1292 clear
    very hard mode lol

---
+++ src/Beakjoon/bj1292/Main.java
@@ -0,0 +1,31 @@
+package Beakjoon.bj1292;
+
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int r = scanner.nextInt();
+        scanner.nextLine();
+        int total = 0;
+        int tmp1 = 1;
+        int radix1 = 1;
+        while (n > tmp1) {
+
+           radix1 += 1;
+           tmp1 += radix1;
+        }
+        for(int i =n; i <= r; i++) {
+            if(i < tmp1) {
+                total += radix1;
+            } else{
+                total += radix1;
+                radix1++;
+                tmp1 += radix1;
+
+            }
+        }
+        System.out.println(total);
+    }
+}
