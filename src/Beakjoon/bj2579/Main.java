55e1d9d8527d1230da4ee38290554a2a0642cc99
Herase <phoebe7075@gmail.com>
2022-01-03 17:51:06 +0900

    2579 solve
    DP의 핵심은 Bottom-Up에서 점화식을 구하는 것. 점화식은 구술 형태로 조건을 주므로 문제를 잘 읽어서 핵심을 이해하는게 필수이다.

--- src/Beakjoon/bj2579/Main.java
+++ src/Beakjoon/bj2579/Main.java
@@ -9,5 +9,17 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String s = bf.readLine();
         int n = Integer.parseInt(s);
+        int arr[] = new int[n+1];
+        int count[] = new int[n+1];
+
+        for(int i=1; i<n+1; i++) {
+            arr[i] = Integer.parseInt(bf.readLine());
+        }
+        count[1] = arr[1];
+        count[2] = arr[1]+arr[2];
+        for(int i=3; i<=n; i++) {
+            count[i] = Math.max(count[i-3]+arr[i-1]+arr[i], count[i-2]+arr[i]);
+        }
+        System.out.println(count[n]);
     }
 }
