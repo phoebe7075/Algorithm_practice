d034974e28c0ef3a7acc98aa29ab83558c210a3e
Herase <phoebe7075@gmail.com>
2022-01-04 22:29:27 +0900

    11724 solve
    solve

---
+++ src/Beakjoon/bj11724/Main.java
@@ -0,0 +1,45 @@
+package Beakjoon.bj11724;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] graph;
+    static boolean[] visit;
+    static int V, E;
+    static int ans = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        graph = new int[V+1][V+1];
+        visit = new boolean[V+1];
+        for(int i=0; i<E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            graph[x][y] = 1;
+            graph[y][x] = 1;
+        }
+        dfs();
+        System.out.println(ans);
+    }
+    static void dfs() {
+        for(int i=1; i < V+1; i++) {
+            if (!visit[i]) {
+                dfs(i);
+                ans++;
+            }
+        }
+    }
+
+    static void dfs(int x) {
+        visit[x] = true;
+        for(int i=1; i < V+1; i++) {
+            if (graph[x][i] != 0 && !visit[i]) {
+                dfs(i);
+            }
+        }
+    }
+}
