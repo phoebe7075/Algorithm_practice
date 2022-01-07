e9de837c88664b45bc5ae39702718339746d02d4
Herase <phoebe7075@gmail.com>
2022-01-07 15:31:31 +0900

    15549 solve
    Integer Minvalue

---
+++ src/Beakjoon/bj15549/Main.java
@@ -0,0 +1,13 @@
+package Beakjoon.bj15549;
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        int x = -2147483648;
+        if (x != 0 && x == -x) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
