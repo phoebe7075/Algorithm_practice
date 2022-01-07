8e244e682b8b70488aef7ef15190525f389e1033
Herase <phoebe7075@gmail.com>
2022-01-07 16:52:13 +0900

    9655, 9656, 9657, 9658 solve
    solve

---
+++ src/Beakjoon/bj9657/Main.java
@@ -0,0 +1,14 @@
+package Beakjoon.bj9657;
+
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if(n%7 == 0 || n%7 == 2) {
+            System.out.println("CY");
+        }else {
+            System.out.println("SK");
+        }
+    }
+}
