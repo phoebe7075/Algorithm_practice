2abf2897185b92f2b817720b7e9706684ede4aa1
Herase <phoebe7075@gmail.com>
2022-01-11 16:21:30 +0900

    11726 solve
    modulo

---
+++ src/Beakjoon/bj11726/Main.java
@@ -0,0 +1,18 @@
+package Beakjoon.bj11726;
+
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long arr[] = new long[1001];
+        arr[1] = 1; arr[2] = 2;
+        if (N > 2) {
+            for(int i=3; i<=N; i++){
+                arr[i] = arr[i-1] + arr[i-2];
+                arr[i] %= 10007;
+            }
+        }
+        System.out.println(arr[N]%10007);
+    }
+}
