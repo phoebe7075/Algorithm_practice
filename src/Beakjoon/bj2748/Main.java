ecc599f2f4c87c23fa28343da9a86fa2ff2ce260
Herase <phoebe7075@gmail.com>
2022-01-24 19:41:17 +0900

    Update Main.java

--- src/Beakjoon/bj2748/Main.java
+++ src/Beakjoon/bj2748/Main.java
@@ -8,12 +8,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         scanner.nextLine();
-        arr = new long[N+1];
-        arr[0] = 0; arr[1] = 1; arr[2] = 1;
-        if(N < 3) {
+        arr = new long[91];
+        arr[0] = 0; arr[1] = 1;
+        if(N < 2) {
             System.out.println(arr[N]);
         }else {
-            for(int i = 3; i <=N; i++) {
+            for(int i = 2; i <=N; i++) {
                 arr[i] = arr[i-1]+arr[i-2];
             }
             System.out.println(arr[N]);
