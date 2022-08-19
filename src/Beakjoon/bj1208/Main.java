4e85fb34f2f6541c777abaed5960caa2101735db
Herase <phoebe7075@gmail.com>
2022-08-19 18:09:22 +0900

    백준 1208 부분수열의 합2 (이진탐색) 해결
    공부 열심히 했다... 진짜 참신한 방법도 봤는데 그걸 배끼는건 양심에 찔려서 못했다...

---
+++ src/Beakjoon/bj1208/Main.java
@@ -0,0 +1,95 @@
+package Beakjoon.bj1208;
+import java.io.*;
+import java.util.*;
+public class Main {
+    static ArrayList<Integer> list = new ArrayList<>();
+    static ArrayList<Integer> list2 = new ArrayList<>();
+    static int[] arr;
+    static boolean zerosum = true;
+    static int n;
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        n = Integer.parseInt(s[0]);
+        int target = Integer.parseInt(s[1]);
+        arr = new int[n];
+        s = br.readLine().split(" ");
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(s[i]);
+        }
+        subsetSum(0, 0, true);
+        zerosum = true;
+        subsetSum(n/2, 0, false);
+        
+        Collections.sort(list);
+        Collections.sort(list2);
+        long ans = 0;
+
+        ans += overlapNumCount(list, target);
+        ans += overlapNumCount(list2, target);
+        for(int i=0; i<list.size(); i++) {
+            int t = target - list.get(i);
+            ans += overlapNumCount(list2, t);
+        }
+        System.out.println(ans);
+    }
+    public static void subsetSum(int idx, int sum, boolean flag){
+        if(flag) {
+            if(idx == n/2) {
+                if(zerosum && sum == 0) {
+                    zerosum = true;
+                    return;
+                }else {
+                    list.add(sum);
+                    return;
+                }
+            }else {
+                subsetSum(idx+1, sum+arr[idx], flag);
+                subsetSum(idx+1, sum, flag);
+            }
+        }else {
+            if(idx == n) {
+                if(zerosum && sum == 0) {
+                    zerosum = true;
+                    return;
+                }else {
+                    list2.add(sum);
+                    return;
+                }
+            }else {
+                subsetSum(idx+1, sum+arr[idx], flag);
+                subsetSum(idx+1, sum, flag);
+            }
+        }
+
+    }
+
+
+    public static int overlapNumCount(ArrayList<Integer> arr, int target){
+        int start = 0, end = arr.size()-1;
+        int mid;
+        while(start <= end) {
+            mid = (start+end)/2;
+            if(arr.get(mid) < target){
+                start = mid+1;
+            }else {
+                end = mid-1;
+            }
+        }
+
+        int lower = start;
+
+        start = 0; end = arr.size()-1;
+
+        while(start <= end) {
+            mid = (start+end)/2;
+            if(arr.get(mid) <= target){
+                start = mid+1;
+            }else {
+                end = mid-1;
+            }
+        }
+
+        return (end-lower+1);
+    }
+}
