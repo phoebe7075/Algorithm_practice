1a1cbfb1686b9c4e08bed2ad18cd2671f86425da
Herase <phoebe7075@gmail.com>
2022-08-29 17:01:01 +0900

    백준 17305 사탕배달 공부
    공부했다

---
+++ src/Beakjoon/bj17305/Main.java
@@ -0,0 +1,48 @@
+package Beakjoon.bj17305;
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int n, w;
+    static ArrayList<Integer> list3 = new ArrayList<>();
+    static ArrayList<Integer> list5 = new ArrayList<>();
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s[] = br.readLine().split(" ");
+        n = Integer.parseInt(s[0]);
+        w = Integer.parseInt(s[1]);
+
+        for(int i=0; i<n; i++) {
+            s = br.readLine().split(" ");
+            if(s[0].equals("3")) {
+                list3.add(Integer.parseInt(s[1]));
+            }else {
+                list5.add(Integer.parseInt(s[1]));
+            }
+        }
+
+        Collections.sort(list3);
+        Collections.sort(list5);
+        long[] sum3 = new long[list3.size()+1];
+        long[] sum5 = new long[list5.size()+1];
+        for(int i=0; i<list3.size(); i++) {
+            sum3[i+1] = sum3[i]+list3.get(i);
+        }
+        for(int i=0; i<list5.size(); i++) {
+            sum5[i+1] = sum5[i]+list5.get(i);
+        }
+
+        int x = Math.min(w/3, list3.size()); // 무게제한에 가장 많이 넣을 수 있는 3그램 사탕의 개수를 구함.
+        long max = 0;
+        while(x >= 0) {
+            int y = Math.min((w-3*x)/5, list5.size()); // 현재 무게에서 3그램 사탕의 개수(x) 를 뺀 뒤 5그램 사탕을 넣을 수 있는 갯수와 5그램 사탕의 총 개수중 적은걸 선택.(인덱스 에러 방지)
+
+            if(max < sum3[x]+sum5[y]) {
+                max = sum3[x]+sum5[y];
+            }
+            x--;
+        }
+        
+        System.out.println(max);
+    }
+}
